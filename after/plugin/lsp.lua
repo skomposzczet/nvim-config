5e53b21aaf8c8a79cd48d25730a82f1509ee19b9
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-05-30 19:23:50 +0200

    reconfigure lsp

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,9 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.setup_servers({'clangd', 'rust_analyzer'})
+
+lsp.setup()
--- lua/skomposzczet/packer.lua
+++ lua/skomposzczet/packer.lua
@@ -24,15 +24,35 @@ return require('packer').startup(function(use)
   use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
   use {
-	  "williamboman/mason.nvim",
-	  run = ":MasonUpdate" -- :MasonUpdate updates registry contents
+	  'goolord/alpha-nvim',
+	  requires = { 'nvim-tree/nvim-web-devicons' },
+	  config = function ()
+		  require'alpha'.setup(require'alpha.themes.startify'.config)
+	  end
   }
-
   use {
-    'goolord/alpha-nvim',
-    requires = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v2.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {                                      -- Optional
+		  'williamboman/mason.nvim',
+		  run = function()
+			  pcall(vim.cmd, 'MasonUpdate')
+		  end,
+	  },
+	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+	  -- Autocompletion
+	  {'hrsh7th/nvim-cmp'},     -- Required
+	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
+	  {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
+
+use {
+	"windwp/nvim-autopairs",
+	config = function() require("nvim-autopairs").setup {} end
 }
 end)
