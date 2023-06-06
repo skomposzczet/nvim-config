30ecfdbcf6533e5244663f6e78277fc657f491e9
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-06-06 18:01:47 +0200

    add remaps + some settings

--- lua/skomposzczet/remap.lua
+++ lua/skomposzczet/remap.lua
@@ -1,2 +1,23 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move command lines selected in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- keep cursor in middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste without changing registers content
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- cool shortcut to sedding current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+
--- lua/skomposzczet/set.lua
+++ lua/skomposzczet/set.lua
@@ -8,6 +8,12 @@ vim.opt.expandtab = true
 
 vim.opt.incsearch = true
 
+vim.opt.termguicolors = true
+
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.list = true
+vim.opt.listchars = "tab:--"
