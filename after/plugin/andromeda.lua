9c96b3877e827a22a3e520480828981f13bcf055
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-06-08 20:15:14 +0200

    add git blame refresh, change theme color

--- after/plugin/andromeda.lua
+++ after/plugin/andromeda.lua
@@ -1,11 +1,11 @@
 require("andromeda").setup({
-	preset = "andromeda",
-	colors = {
-		background = "#15171C",
-		mono_1     = "#2f323c", -- secondary background and/or highlighting 
-		mono_2     = "#3a3e4b", -- used for highlighting 
-		mono_3     = "#464959", -- used for highlighting 
-		mono_4     = "#a0a1a7", -- comments 
-		mono_5     = "#d5ced9", -- normal text
-	}
+    preset = "andromeda",
+    colors = {
+        background = "#23262e",
+        mono_1     = "#2f323c", -- secondary background and/or highlighting 
+        mono_2     = "#3a3e4b", -- used for highlighting 
+        mono_3     = "#464959", -- used for highlighting 
+        mono_4     = "#a0a1a7", -- comments 
+        mono_5     = "#d5ced9", -- normal text
+    }
 })
---
+++ after/plugin/git-blame.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("n", "<leader>gb", function()
+    vim.cmd("GitBlameToggle")
+    vim.cmd("GitBlameToggle")
+end)
+
