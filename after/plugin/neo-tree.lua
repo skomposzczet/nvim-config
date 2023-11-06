0ee6b47fa0dfb2fbb68e6f3dea65f9f07f1edea0
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-11-06 22:54:32 +0100

    add neo tree

---
+++ after/plugin/neo-tree.lua
@@ -0,0 +1,29 @@
+require("neo-tree").setup({
+    window = {
+        position = "current",
+        mappings = {
+            ["="] = "open",
+            ["l"] = "open",
+            ["h"] = "close_node",
+        }
+    },
+    hijack_netrw_behavior = "open_default",
+    filesystem = {
+        filtered_items = {
+            visible = true,
+            hide_gitignored = true,
+        }
+    },
+    name = {
+        trailing_slash = true,
+    },
+    popup_border_style = "rounded",
+})
+
+-- disable netrw at the very start of your init.lua
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+
+vim.keymap.set("n", "<leader>pv", function()
+    vim.cmd("Neotree position=float toggle reveal")
+end)
